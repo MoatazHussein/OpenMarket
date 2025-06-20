54b5998873e441e9683440666aa7927b093c9e4f
moAhmed1195 <57796466+moAhmed1195@users.noreply.github.com>
2025-06-20 12:56:02 +0300

    AddSafteyRules

--- src/app/modules/layout/footer/footer-pages/safety-rules/safety-rules/safety-rules.component.ts
+++ src/app/modules/layout/footer/footer-pages/safety-rules/safety-rules/safety-rules.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { PageSection } from '../../../../../../models/page-section.model';
+import { PageSectionService } from '../../../../../../Services/page-section.service';
+import { LanguageService } from '../../../../../../core/services/language.service';
 
 @Component({
   selector: 'app-safety-rules',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrl: './safety-rules.component.css'
 })
 export class SafetyRulesComponent {
+ pageKey: string = '7';
+  pageHeader: string = '';
+  pageSubHeader: string = '';
+  currentLang: string = 'ar';
+  isRtl: Boolean = true;
 
+  sections: PageSection[] = [];
+  constructor(
+    private sectionService: PageSectionService, private languageService: LanguageService,) {
+  }
+  ngOnInit() {
+    this.languageService.language$.subscribe(lang => {
+      this.currentLang = lang;
+    });
+
+    if (this.pageKey) {
+      this.sectionService.getSectionsByPageKey(this.pageKey).subscribe(data => {
+        this.sections = data.sort((a, b) => a.sectionOrder - b.sectionOrder);
+      });
+      if (this.currentLang == 'ar') {
+        this.isRtl = true;
+        this.pageHeader = 'قواعد السلامة'   
+
+        this.pageSubHeader = ''
+      }
+      else {
+        this.isRtl = false;
+        this.pageHeader = 'Safety Rules'
+        this.pageSubHeader = ''
+      }
+    }
+  }
 }
